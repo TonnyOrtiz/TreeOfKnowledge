35f764dbfc7f162dc525917f6fbf552d3748b996
TonnyOrtiz <tonny.ortiz.95@gmail.com>
2023-02-02 04:08:34 -0600

    Updating everything so it works on web

--- flutter/brew_app/lib/main.dart
+++ flutter/brew_app/lib/main.dart
@@ -4,10 +4,13 @@ import 'package:brew_app/services/auth.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-Future <void> main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -26,4 +29,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
--- flutter/world_time/lib/services/world_time.dart
+++ flutter/world_time/lib/services/world_time.dart
@@ -1,45 +1,42 @@
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:intl/intl.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class WorldTime {
-
   String location; // Location name for the UI
-  String time=''; // Time in that location
+  String time = ''; // Time in that location
   String flag; // Url to an asset image
-  String url;  // location url for api endpoint
-  bool isDaytime=true; // true or false if is daytime or not
+  String url; // location url for api endpoint
+  bool isDaytime = true; // true or false if is daytime or not
 
-  WorldTime({this.location = 'Costa Rica', this.flag = '', this.url='America/Costa_Rica'});
+  WorldTime(
+      {this.location = 'Costa Rica',
+      this.flag = '',
+      this.url = 'America/Costa_Rica'});
 
   Future<void> getTime() async {
-
     try {
+      print('parada 1\n');
+      Response response;
       // Make the request
-      Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-      Map data = jsonDecode(response.body);
-      //print(data);
-      String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(1,3);
-      String offsetSign = data['utc_offset'].substring(0,1);
-      // true si es positivo, false si es negativo
-      bool addoffset = offsetSign.compareTo('+') == 0 ? true : false;
-
-      DateTime now = DateTime.parse(datetime);
-      if (addoffset) {
-        now = now.add(Duration(hours: int.parse(offset)));
+      if (kIsWeb) {
+        response = await get(Uri.parse(
+            'https://cors-anywhere.herokuapp.com/https://www.timeapi.io/api/Time/current/zone?timeZone=$url'));
       } else {
-        now = now.subtract(Duration(hours: int.parse(offset)));
+        response = await get(Uri.parse(
+            'https://www.timeapi.io/api/Time/current/zone?timeZone=$url'));
       }
+      Map data = jsonDecode(response.body);
+      print(data);
+      time = data['dateTime'];
+      DateTime now = DateTime.parse(time);
       // Set the time property
       isDaytime = now.hour > 4 && now.hour < 19 ? true : false;
-      //isDaytime = false;
       time = DateFormat.jm().format(now);
-
-    } catch(e) {
+    } catch (e) {
       //print('Error: $e');
       time = 'Error';
     }
-
   }
 }
